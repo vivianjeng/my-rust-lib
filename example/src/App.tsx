fda40a47e0eddb2a9c1a02f85fe2e4253d0edc9d
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2025-10-07 12:04:34 +0800

    feat: ios works

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,49 @@
 import { Text, View, StyleSheet } from 'react-native';
-import { multiply } from 'react-native-my-rust-lib';
+import {
+  Calculator,
+  type BinaryOperator,
+  SafeAddition,
+  ComputationResult,
+  greet,
+} from 'my-rust-lib';
+import { useEffect, useState } from 'react';
 
-const result = multiply(3, 7);
+// A Rust object
+const calculator = new Calculator();
+// A Rust object implementing the Rust trait BinaryOperator
+const addOp = new SafeAddition();
+
+// A Typescript class, implementing BinaryOperator
+class SafeMultiply implements BinaryOperator {
+  perform(lhs: bigint, rhs: bigint): bigint {
+    return lhs * rhs;
+  }
+}
+const multOp = new SafeMultiply();
+
+// bigints
+const three = 3n;
+const seven = 7n;
+
+// Perform the calculation, and to get an object
+// representing the computation result.
+const computation: ComputationResult = calculator
+  .calculate(addOp, three, three)
+  .calculateMore(multOp, seven)
+  .lastResult()!;
+
+// Unpack the bigint value into a string.
+const result = computation.value.toString();
 
 export default function App() {
+  const [greeting, setGreeting] = useState('');
+  useEffect(() => {
+    setGreeting(greet('Vivian'));
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
+      <Text>Greeting: {greeting}</Text>
     </View>
   );
 }
